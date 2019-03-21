d310cdb140469f6f6681dafc15ad529dffed4fb4
Anton Stagge <antonstagge95@gmail.com>
2019-03-21 20:25:29 +0100

    Done with point 5 accuracy

--- assignment1/main.py
+++ assignment1/main.py
@@ -7,15 +7,15 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
-
 def make_one_hot(y):
+    """ create one-hot column vectors """
     one_hot = np.zeros((len(y), 10))
     for i in range(len(y)):
         one_hot[i, y[i]] = 1.
     return one_hot.transpose(1,0)
 
 def load_batch(batch_name):
-    """ Returns 
+    """  
     X = (3072, 10000) each column is an image
     Y = (10, 10000) each colum is a one hot vector
     y = labels for each column
@@ -28,6 +28,8 @@ def load_batch(batch_name):
     return X, Y, y
 
 def visulize_5(X):
+    """ Show 5x5 images from X
+    """
     fig, axes1 = plt.subplots(5,5,figsize=(3,3))
     for j in range(5):
         for k in range(5):
@@ -36,12 +38,63 @@ def visulize_5(X):
             axes1[j][k].imshow(X[:,i].reshape(32, 32, 3))
     plt.show()
 
+def softmax(s):
+    """Compute softmax values for each sets of scores in s"""
+    exps = np.exp(s)
+    return exps / np.sum(exps, axis=0)
+
+def evaluate_classifier(X, W, b):
+    """
+    W = (10,3072)
+    X = (3072, n)
+    p = (10, n)
+    """
+    n = X.shape[1]
+    WX = np.matmul(W, X)
+    b_big = np.repeat(b, n, axis=1) # repeat column vector b, n times
+    s = WX + b_big
+    p = softmax(s)
+    return p
+
+def compute_cost(X, Y, W, b, _lambda):
+    """
+    W = (10,3072)
+    X = (3072, n)
+    p = (10, n)
+    """
+    n = X.shape[1]
+    p = evaluate_classifier(X, W, b) #
+
+    py = np.matmul(Y.transpose(), p)  # (n, n)
+    cross_entropy = -np.log(py) # (n, n)
+    regulation = _lambda * np.sum(W**2) # scalar
+    J = np.sum(cross_entropy) / n + regulation # scalar
+    return J
+
+def predict(p):
+    return np.argmax(p, axis=0).reshape(1, p.shape[1])
+
+def compute_accuracy(X, y, W, b):
+    p = evaluate_classifier(X, W, b)
+    predicted = predict(p)
+
+    zero_one_loss = 0
+    for i in range(X.shape[1]):
+        if predicted[0, i] != y[i]:
+            zero_one_loss += 1
+    
+    accuracy = 1 - zero_one_loss / X.shape[1]
+    
+
 if __name__ == '__main__':
     X, Y, y = load_batch('data_batch_1')
     # visulize_5(X)
     K = 10
-    n = 10000
-    d = X.shape[1]
+    n_tot = 10000
+    d = 3072
 
     W = np.random.normal(0, 0.1, size=(K, d))
     b = np.random.normal(0, 0.1, size=(K, 1))
+
+    # cost = compute_cost(X, Y, W, b, 0.1)
+    compute_accuracy(X, y, W, b)
