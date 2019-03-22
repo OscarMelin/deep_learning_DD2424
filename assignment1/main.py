0b52cc5b76c94c55c47e241ddf05b3b5aa7e5470
Anton Stagge <antonstagge95@gmail.com>
2019-03-22 09:29:00 +0100

    Added fast numerical gradient function

--- assignment1/main.py
+++ assignment1/main.py
@@ -7,6 +7,33 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
+def compute_grads_num(X, Y, W, b, _lambda, h):
+    no = W.shape[0]
+    d = X.shape[0]
+
+    grad_W = np.zeros_like(W)
+    grad_b = np.zeros((no,1))
+    grad_b_2 = np.zeros((no,1))
+
+    c = compute_cost(X, Y, W, b, _lambda)
+
+    print("Starting with b")
+    for i in range(b.shape[0]):
+        b_try = b
+        b_try[i] = b_try[i] + h
+        c2 = compute_cost(X, Y, W, b_try, _lambda)
+        grad_b[i] = (c2-c) /h
+
+    print("Starting with W")
+    for i in range(W.shape[0]):
+        for j in range(W.shape[1]):
+            W_try = W
+            W_try[i][j] = W_try[i][j] + h
+            c2 = compute_cost(X, Y, W_try, b, _lambda)
+            grad_W[i][j] = (c2-c) /h
+    
+    return grad_W, grad_b
+
 def make_one_hot(y):
     """ create one-hot column vectors """
     one_hot = np.zeros((len(y), 10))
@@ -84,6 +111,9 @@ def compute_accuracy(X, y, W, b):
             zero_one_loss += 1
     
     accuracy = 1 - zero_one_loss / X.shape[1]
+
+def compute_gradients(X, Y, P, W, _lambda):
+    print("")
     
 
 if __name__ == '__main__':
@@ -97,4 +127,7 @@ if __name__ == '__main__':
     b = np.random.normal(0, 0.1, size=(K, 1))
 
     # cost = compute_cost(X, Y, W, b, 0.1)
-    compute_accuracy(X, y, W, b)
+    # compute_accuracy(X, y, W, b)
+    num_grad_W, num_grad_b = compute_grads_num(X[:,:20], Y[:,:20], W, b, 0, 1e-6)
+    print(num_grad_W)
+    print(num_grad_b)
